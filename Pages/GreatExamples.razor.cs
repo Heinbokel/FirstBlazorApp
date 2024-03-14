aaca59b26b440e06b0ddbfad7220325ed5426cc8
Robert Heinbokel <robertheinbokel@gmail.com>
2024-03-14 16:08:01 -0400

    More on part 2 of blazor and razor

---
+++ Pages/GreatExamples.razor.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Components;
+
+namespace FirstBlazorApp.Pages;
+
+public partial class GreatExamples: ComponentBase {
+    private string PageTitle {get; set;} = "Great Examples in Blazor";
+}
